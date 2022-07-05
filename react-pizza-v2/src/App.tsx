2cd13ab4a100dc8a69a2d7d3e720a55d101311c2
Nikita <66018338+kolomyaka1@users.noreply.github.com>
2022-07-05 19:15:24 +0300

    Создание проекта

---
+++ react-pizza-v2/src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Header } from './components/Header';
+import { Categories } from './components/Categories';
+import './scss/app.scss';
+import { Sort } from './components/Sort';
+import { PizzaBlock } from './components/PizzaBlock';
+
+function App() {
+    return (
+        <>
+            <div className="wrapper">
+                <Header />
+                <div className="content">
+                    <div className="container">
+                        <div className="content__top">
+                            <Categories />
+                            <Sort />
+                        </div>
+                        <h2 className="content__title">Все пиццы</h2>
+                        <div className="content__items">
+                            <PizzaBlock title={'Мексиканская'} price={500} />
+                            <PizzaBlock title={'Четыре Сыра'} price={350} />
+                            <PizzaBlock title={'Пепперони'} price={550} />
+                            <PizzaBlock title={'Гавайская'} price={400} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default App;
